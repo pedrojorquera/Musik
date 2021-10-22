dfa93f01048aa2dfed81b3a643fc9386663bee0e
pedrojorquera <pedrrojh@gmail.com>
2021-10-22 13:17:44 +0200

    Subida inicial del proyecto

---
+++ src/musik/sugerencias/CancionSugerida.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musik.sugerencias;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+        
+/**
+ *
+ * @author PedroJorqueraHoyas
+ */
+public class CancionSugerida {
+    private Date fecha;
+    private int reproducciones;
+    private String nombreCancion;
+    public CancionSugerida() {
+    }
+
+    public CancionSugerida(Date fecha, int reproducciones, String nombreCancion) {
+        this.fecha = fecha;
+        this.reproducciones = reproducciones;
+        this.nombreCancion = nombreCancion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getReproducciones() {
+        return reproducciones;
+    }
+
+    public void setReproducciones(int reproducciones) {
+        this.reproducciones = reproducciones;
+    }
+
+    public String getNombreCancion() {
+        return nombreCancion;
+    }
+
+    public void setNombreCancion(String nombreCancion) {
+        this.nombreCancion = nombreCancion;
+    }
+    
+    
+}
